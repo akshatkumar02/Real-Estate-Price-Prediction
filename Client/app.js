73e33806a487f9d34e6392471141da68720eed97
akshatkumar02 <166061798+akshatkumar02@users.noreply.github.com>
2025-07-02 12:30:32 +0530

    Add files via upload

---
+++ Client/app.js
@@ -0,0 +1,68 @@
+function getBathValue() {
+  var uiBathrooms = document.getElementsByName("uiBathrooms");
+  for (let i = 0; i < uiBathrooms.length; i++) {
+    if (uiBathrooms[i].checked) {
+      return i + 1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function getBHKValue() {
+  var uiBHK = document.getElementsByName("uiBHK");
+  for (let i = 0; i < uiBHK.length; i++) {
+    if (uiBHK[i].checked) {
+      return i + 1;
+    }
+  }
+  return -1; // Invalid Value
+}
+
+function onClickedEstimatePrice() {
+  console.log("Estimate price button clicked");
+  var sqft = document.getElementById("uiSqft");
+  var bhk = getBHKValue();
+  var bathrooms = getBathValue();
+  var location = document.getElementById("uiLocations");
+  var estPrice = document.getElementById("uiEstimatedPrice");
+
+  var url = "http://127.0.0.1:5000/predict_home_price";
+
+  $.ajax({
+    url: url,
+    type: "POST",
+    data: {
+      total_sqft: parseFloat(sqft.value),
+      bhk: bhk,
+      bath: bathrooms,
+      location: location.value
+    },
+    success: function(data) {
+      console.log(data.estimated_price);
+      estPrice.innerHTML = "<h2>" + data.estimated_price.toString() + " Lakh</h2>";
+    },
+    error: function(xhr, status, error) {
+      console.error("Error occurred:", status, error);
+      estPrice.innerHTML = "<h2>Error fetching price</h2>";
+    }
+  });
+}
+
+function onPageLoad() {
+  console.log("document loaded");
+  var url = "http://127.0.0.1:5000/get_location_names";
+  $.get(url, function(data, status) {
+    console.log("got response for get_location_names request");
+    if (data) {
+      var locations = data.locations;
+      var uiLocations = document.getElementById("uiLocations");
+      $('#uiLocations').empty();
+      for (var i in locations) {
+        var opt = new Option(locations[i]);
+        $('#uiLocations').append(opt);
+      }
+    }
+  });
+}
+
+window.onload = onPageLoad;
